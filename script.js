5dc2283c5c732462ae2849f65f06c8847407c07c
jnfrap <jnfr97@gmail.com>
2025-05-03 03:26:07 +0200

    Scripts mixed, deleted index.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 /* Functionality viewer */
 
 const buttons = document.querySelectorAll('.functionality-viewer-button');
@@ -12,4 +11,40 @@ buttons.forEach((button, index) => {
             func.style.display = index === funcIndex ? 'block' : 'none';
         });
     });
+});
+
+
+/* Faq */
+
+const button1 = document.querySelector(".q1 button");
+const button2 = document.querySelector(".q2 button");
+const button3 = document.querySelector(".q3 button");
+const images = document.querySelectorAll(".faq-answers-container img");
+
+// Funciones reutilizables
+const hideAllImages = () => { 
+  images.forEach((image) => {
+    image.classList.add("hidden");
+  });  
+}
+
+const showImageFor = (id) => {
+  const image = document.querySelector(id);
+  image.classList.remove("hidden");
+}
+
+// Eventos
+button1.addEventListener("click", () => {
+  hideAllImages();
+  showImageFor("#q1");
+})
+
+button2.addEventListener("click", () => {
+  hideAllImages();
+  showImageFor("#q2");
+});
+
+button3.addEventListener("click", () => {
+  hideAllImages();
+  showImageFor("#q3");
 });
