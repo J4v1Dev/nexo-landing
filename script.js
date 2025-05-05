a716e70c855c4774458b7604720fcb824203c389
jnfrap <jnfr97@gmail.com>
2025-05-05 14:48:55 +0200

    Removed unused code

--- script.js
+++ script.js
@@ -12,39 +12,3 @@ buttons.forEach((button, index) => {
         });
     });
 });
-
-
-/* Faq */
-
-const button1 = document.querySelector(".q1 button");
-const button2 = document.querySelector(".q2 button");
-const button3 = document.querySelector(".q3 button");
-const answers = document.querySelectorAll(".faq-answers-container .faq-answer");
-
-// Funciones reutilizables
-const hideAllAnswers = () => { 
-  answers.forEach((answer) => {
-    answer.classList.add("hidden");
-  });  
-}
-
-const showAnswerFor = (id) => {
-  const answer = document.querySelector(id);
-  answer.classList.remove("hidden");
-}
-
-// Eventos
-button1.addEventListener("click", () => {
-  hideAllAnswers();
-  showAnswerFor("#q1");
-});
-
-button2.addEventListener("click", () => {
-  hideAllAnswers();
-  showAnswerFor("#q2");
-});
-
-button3.addEventListener("click", () => {
-  hideAllAnswers();
-  showAnswerFor("#q3");
-});
